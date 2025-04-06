78e9f650765d7f0a7350bbdd550d03a7c60d3f52
Deepak Maurya <deepakmaurya1378@gmail.com>
2025-04-06 20:27:58 +0530

    extra text

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -38,7 +38,7 @@ function Dashboard() {
         </p>
       </div>
 
-      {/* Add New Interview Section */}
+       {/* Add New Interview Section  */}
       <section className="mb-10">
         <h3 className="text-2xl font-semibold text-gray-700 mb-6">Create New Interview</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
